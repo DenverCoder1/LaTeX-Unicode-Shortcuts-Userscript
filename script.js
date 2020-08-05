378718f8ec8b9fa821aef489cc2d378c2aa06711
eyl327 <jonah@freshidea.com>
2020-08-06 00:00:28 +0300

    Added support for "content editable" elements (supports Gmail, Youtube, ...)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LaTeX Unicode Shortcuts
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  Highlight text then press [ALT+X] to convert LaTeX commands to their unicode equivalent (ex. \pi → π)
 // @author       eyl327
 // @match        *://*/*
@@ -24,7 +24,7 @@
         xhr.open("GET", url, false);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
-                if (xhr.status === 200 || xhr.status == 0) {
+                if (xhr.status === 200 || xhr.status === 0) {
                     callback(xhr.responseText);
                 }
             }
@@ -58,7 +58,7 @@
     }
 
     /* get caret position within input box */
-    function getCaretPosition(el) {
+    function getCaretPositionInputBox(el) {
         if ("selectionStart" in el && document.activeElement == el) {
             return {
                 start: el.selectionStart,
@@ -103,55 +103,158 @@
         }
     }
 
-    function replaceConversionInElement(activeEl, start, end) {
-        var fullText = activeEl.value;
+    function overwriteInputBoxText(activeEl, before, convertedText, after) {
+        // overwrite text
+        activeEl.value = before + convertedText + after;
+        // set cursor to be at end of selection
+        setCaretPosition(activeEl, before.length + convertedText.length);
+    }
+
+    function replaceConversionInElement(activeEl, fullText, start, end) {
         var textToConvert = fullText.substring(start, end);
         var before = fullText.substring(0, start);
         var after = fullText.substring(end, fullText.length);
         // convert selection
         var convertedText = convert(textToConvert);
-        // overwrite text
-        activeEl.value = before + convertedText + after
-        // set cursor to be at end of selection
-        setCaretPosition(activeEl, before.length + convertedText.length);
+        if ("value" in activeEl) {
+            overwriteInputBoxText(activeEl, before, convertedText, after);
+        }
     }
 
     /* convert hilighted text in active element */
-    function convertSelection(activeEl) {
-        var caretRange = getCaretPosition(activeEl);
+    function convertSelectionInputBox(activeEl) {
+        var caretRange = getCaretPositionInputBox(activeEl);
         var selStart = caretRange.start;
         var selEnd = caretRange.end;
+        var fullText = activeEl.value;
         /* if selection is empty, find word at caret */
         if (selStart == selEnd) {
-            var fullText = activeEl.value;
             // Find beginning and end of word
             var left = fullText.slice(0, selStart + 1).search(/\S+$/);
             var right = fullText.slice(selStart).search(/(\s|$)/);
             /* convert the word at the caret selection */
-            replaceConversionInElement(activeEl, left, right + selStart)
+            replaceConversionInElement(activeEl, fullText, left, right + selStart)
         }
         /* else convert the selection */
         else {
-            replaceConversionInElement(activeEl, selStart, selEnd);
+            replaceConversionInElement(activeEl, fullText, selStart, selEnd);
         }
     }
 
+    /* convert hilighted text in active element */
+    function convertSelectionContentEditable(element) {
+        var NodeTree = {
+            // Used to find all DOM nodes in window.getSelection()
+            getInnerNodes: function (anchor, focus) {
+                var ancestor = NodeTree.lowestCommonAncestor(anchor, focus);
+                var childList = NodeTree.findChildrenList(ancestor);
+                var [i, j] = [childList.indexOf(anchor), childList.indexOf(focus)].sort();
+                return childList.slice(i, j + 1);
+            },
+            getNodeChain: function (node) {
+                var chain = [];
+                chain.push(node);
+                while (node.parentNode) {
+                    node = node.parentNode;
+                    chain.push(node);
+                }
+                return chain.reverse();
+            },
+            lowestCommonAncestor: function (anchor, focus) {
+                var uChain = NodeTree.getNodeChain(anchor);
+                var vChain = NodeTree.getNodeChain(focus);
+                var i;
+                for (i = 0; i < uChain.length; i++) {
+                    if (uChain[i] !== vChain[i]) {
+                        break
+                    }
+                }
+                return uChain[i - 1]
+            },
+            findChildrenList: function (node) {
+                var list = []
+                var find = function (n) {
+                    if (!n) {
+                        return;
+                    }
+                    list.push(n);
+                    for (var child of Array.from(n.childNodes || [])) {
+                        find(child);
+                    }
+                }
+                find(node);
+                return list;
+            }
+        }
+
+        var sel = element.ownerDocument.getSelection();
+
+        var selAN = sel.anchorNode;
+        var selFN = sel.focusNode;
+
+        var nodesBetweenNodes = NodeTree.getInnerNodes(selAN, selFN);
+
+        var startNode = nodesBetweenNodes[0];
+        var endNode = nodesBetweenNodes[nodesBetweenNodes.length - 1];
+
+        var selAO = sel.anchorOffset;
+        var selFO = sel.focusOffset;
+
+        var [startCursor, endCursor] = (startNode === selAN && selAO <= selFO) ? [selAO, selFO] : [selFO, selAO];
+
+        var cursor;
+
+        for (var node of nodesBetweenNodes) {
+            if (node.nodeType === 3) { // 3 = text type
+                var selStart = (node === nodesBetweenNodes[0]) ? startCursor : 0;
+                var selEnd = (node === nodesBetweenNodes[nodesBetweenNodes.length - 1]) ? endCursor : node.nodeValue.length;
+                var text = node.nodeValue;
+                selEnd = Math.min(text.length, selEnd);
+
+                var convertStart = selStart;
+                var convertEnd = selEnd;
+
+                // cursor is not a hilighted selection
+                if (selStart == selEnd) {
+                    // Find beginning and end of word
+                    convertStart = text.slice(0, selStart + 1).search(/\S+$/);
+                    convertEnd = text.slice(selEnd).search(/(\s|$)/) + selStart;
+                }
+
+                /* convert the word at the caret selection */
+                var textToConvert = text.substring(convertStart, convertEnd);
+                var before = text.substring(0, convertStart);
+                var after = text.substring(convertEnd, text.length);
+                var convertedText = convert(textToConvert);
+
+                // replace in element
+                var result = before + convertedText + after;
+                cursor = Math.min(result.length, before.length + convertedText.length);
+                node.nodeValue = result;
+            }
+        }
+        sel.collapse(endNode, cursor)
+    }
+
     /* detect ALT+X keyboard shortcut */
-    async function enableLaTeXShortcuts(event) {
+    function enableLaTeXShortcuts(event) {
         if (event.altKey && event.keyCode == 88) { // ALT+X
             // load dictionary when first pressed
             if (!dictLoaded) {
-                await loadAsset(dictionarySource, loaded);
+                loadAsset(dictionarySource, loaded);
             }
             // convert selection
             var activeEl = document.activeElement;
             var activeElTag = activeEl.tagName.toLowerCase();
             if (activeElTag == "textarea" || activeElTag == "input") {
-                convertSelection(activeEl);
+                convertSelectionInputBox(activeEl);
+            }
+            else if (activeEl.contentEditable) {
+                convertSelectionContentEditable(activeEl);
             }
         }
     }
 
-    document.addEventListener('keydown', enableLaTeXShortcuts, false);
+    document.addEventListener('keydown', enableLaTeXShortcuts, true);
 
 })();
